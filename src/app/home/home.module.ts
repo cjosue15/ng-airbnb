30c1bcbdcdb9af30af580b44221c1d479142b412
Carlos Morales <carlosjosuemn98@gmail.com>
2021-08-26 11:25:20 -0500

    banner was added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { ExploreComponent } from './components/explore/explore.component';
 import { LiveWorldComponent } from './components/live-world/live-world.component';
 import { HostComponent } from './components/host/host.component';
 import { ActivitiesComponent } from './components/activities/activities.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 const routes: Routes = [
   {
@@ -15,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [HomeComponent, ExploreComponent, LiveWorldComponent, HostComponent, ActivitiesComponent],
+  declarations: [HomeComponent, ExploreComponent, LiveWorldComponent, HostComponent, ActivitiesComponent, BannerComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
